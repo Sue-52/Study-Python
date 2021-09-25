54750b9254ea668cf98b876bcae8100ead90f447
Sue-52 <1219243947@qq.com>
2021-09-25 10:51:33 +0800

    submit：python study books

---
+++ 1.Python 基础/day06-基础学习/code/fn.py
@@ -0,0 +1,24 @@
+# def addNum():
+#   print("函数测试")
+# addNum()
+
+# def compMax(a,b):
+#     if(a<b):
+#         print(b,"--b--打")
+#     else:
+#         print(a,"--a--打")
+
+# compMax(1, 21)  #2 1 --b--打
+# compMax(19, 8)  # 19 --a--打
+
+# def test():
+#   """测试注释"""
+#   print("Hello Function")
+# test()
+# help(test.__doc__)
+
+def fnAdd(a,b):
+    return a+b
+
+c = fnAdd(1,5)
+print(c)  # 6
